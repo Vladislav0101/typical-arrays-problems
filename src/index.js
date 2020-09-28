6d87f19689e2f7e008c42d7c11a42ddc41f643f6
Vladislav0101 <by.vlad01@mail.ru>
2020-09-28 22:39:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+const { sort } = require("semver");
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || array.length == 0) {
+        return 0;
+    } else {
+        return Math.min.apply(null, array);
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length == 0) {
+        return 0;
+    } else {
+        return Math.max.apply(null, array);
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length == 0) {
+        return 0;
+    } else {
+        return (array.length === 0) || (!array) ? 0 : array.reduce((sum, current) => (sum + current), 0) / array.length;
+    }
 }
